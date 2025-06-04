aa3c44cd8ff7ed959b9820a2cfcd7978cb01bae9
Lucas <lukass.mc348@gmail.com>
2025-06-04 13:03:14 -0300

    Catalogo funcionando

--- public/catalogo.js
+++ public/catalogo.js
@@ -1,6 +1,5 @@
 
-
-const btn = document.getElementById('btn-catalogo');
+const btn = document.getElementsByClassName('scroll-btn');
 
 const getdata = async (url) => {
     let response = await fetch(url);
@@ -10,29 +9,34 @@ const getdata = async (url) => {
     renderCatalogo(catalogo);
 }
 
-function renderCatalogo(catalogo) {
-    let container = document.getElementById('juegos-container');
-    const ul = createElement('ul');
-    ul.className = 'list-juegos';
-    container.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevos elementos
-    catalogo.forEach((juego) => {
-        const item = createElement('li');
-        item.className = 'list-group-item';
-        item.innerHTML = `
-            <h5>${juego.nombre}</h5>
-            <p>Precio: $${juego.precio}</p>
-            <p>Descripción: ${juego.lanzamiento}</p>
-            <img src="${juego.imagen}" alt="${juego.nombre}" class="img-fluid">
-        `;
-        ul.appendChild(item);
-    });
+function renderCatalogo(catalogo){
+    let cardHtml= ""
 
-    container.appendChild(ul);
+    
+    catalogo.forEach((juego) =>{
+        const { nombre, tipo, plataforma, precio, imagen_url } = juego;
+
+    cardHtml = `
+      <div class="col-md-4">
+        <div class="card h-100 text-white bg-dark shadow">
+          <img src="${imagen_url}" class="card-img-top" alt="${nombre}">
+          <div class="card-body">
+            <h5 class="card-title">${nombre}</h5>
+            <p class="card-text"><strong>Tipo:</strong> ${tipo}</p>
+            <p class="card-text"><strong>Consolas:</strong> ${plataforma}</p>
+            <p class="card-text"><strong>Precio:</strong> $${precio}</p>
+          </div>
+          <div class="card-footer bg-transparent border-top-0">
+            <button class="btn btn-warning w-100">Agregar al carrito</button>
+          </div>
+        </div>
+      </div>
+      `;
+      document.getElementById("productCards").insertAdjacentHTML("beforeend", cardHtml);
+
+ });
 }
 
+getdata("http://localhost:3000/catalogo");
 
-//
-btn.addEventListener('click', () => {
-    getdata("http://localhost:3000/catalogo");
 
-});
