c2993f69c002d3147e408243b5e70fa523f71f12
lukass.mc <159139968+lukassmc@users.noreply.github.com>
2025-06-27 13:05:00 -0300

    Admin panels creado, falta funcionalidad

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,10 @@ import sequelize from './config/db-sequalize.js';
 import {join, __dirname} from './utils/index.js'
 import cors from 'cors'
 
+import path from 'path';
+import fs from 'fs';
+import ejs from 'ejs';
+
 //settings
 const app= express();
 const port = 3000
@@ -28,6 +32,15 @@ app.use("/products", ProductRouter);
 app.use("/users", UserRouter);
 
 
+
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "../views"));
+
+app.get("/admin", async (req, res) => {
+  const contenido = fs.readFileSync(join(__dirname, "../views/admin.ejs"), "utf8");
+  const body = ejs.render(contenido);
+  res.render("index", { title: "Panel de administración", body });
+});
 
 
 ///Inicializacion del servidor
